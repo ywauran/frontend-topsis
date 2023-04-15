60c87e2f54a922b0ebb8a56b7c6a25ef41ae596e
Yohanes Harke Wauran <harkewauran23@gmail.com>
2023-04-15 19:20:50 +0800

    fix login page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-// import Login from "./pages/Login";
-// import MainAdmin from "./pages/admin/MainAdmin";
-// import MainUser from "./pages/user/MainUser";
+import Login from "./pages/Login";
+import MainAdmin from "../src/pages/admin/MainAdmin";
+import MainUser from "../src/pages/user/MainUser";
 
 const App = () => {
   return (
     <>
       <Routes>
-        {/* <Route path="/" element={<MainUser />} />
-        <Route path="/*" element={<MainUser />} />
+        <Route path="/" element={<MainUser />} />
+        {/* <Route path="/*" element={<MainUser />} />
         <Route path="/login" element={<Login />} />
         <Route path="/pages/*" element={<MainAdmin />} /> */}
-        <Route path="/" element={<h1>Hello</h1>} />
       </Routes>
     </>
   );
